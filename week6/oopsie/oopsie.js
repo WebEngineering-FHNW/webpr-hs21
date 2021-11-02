cd239494dd25e494a3a066c4b023568f051ffb1d
Dierk Koenig <dierk.koenig@canoo.com>
2021-11-02 16:02:15 +0100

    oopsie done

--- week6/oopsie/oopsie.js
+++ week6/oopsie/oopsie.js
@@ -8,7 +8,19 @@
 //   proceed(stride) // proceed so many places
 //   fallback()      // "oopsie": go back to last start (fallback position)
 //   turn()          // cash in your win, update fallback position for next turn
-// 
+//
+
+const Player = name => {
+    let fallbackIndex = 0;
+    let progressIndex = 0;
+    return {
+        getFallbackIndex : () => fallbackIndex,
+        getProgressIndex : () => progressIndex,
+        proceed : stride => progressIndex += stride,
+        fallback: ()     => progressIndex = fallbackIndex,
+        turn:     ()     => fallbackIndex = progressIndex
+    }
+}
 
 function start() {
     const fields = document.getElementById('fields');
