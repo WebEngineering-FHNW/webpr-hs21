5924d16b3187706ed9af45c7654266ef56eb932a
Dierk Koenig <dierk.koenig@canoo.com>
2021-11-09 16:43:25 +0100

    todo: first static todo can be added

--- week8/todo/todo.js
+++ week8/todo/todo.js
@@ -4,5 +4,12 @@ function startTodo() {
 }
 
 function addTodo() {
-    
+    const container = document.getElementById("todoContainer");
+    container.innerHTML = `<tr>
+            <td>
+                <label for="todo0">First Todo:</label>
+                <input type="text" name="todo0" id="todo0" value="my first todo!">
+            </td>
+        </tr>`
+
 }
