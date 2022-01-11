e908396812987be48351fd8818fc34ff0dbf2b67
Dierk Koenig <dierk.koenig@canoo.com>
2022-01-11 17:19:38 +0100

    week 14:  callback, splice tested

--- week14/src/refresherTest.js
+++ week14/src/refresherTest.js
@@ -1,4 +1,5 @@
 import {TestSuite} from "./kolibri/util/test.js";
+import "./kolibri/util/array.js"
 
 const testSuite = TestSuite("refresher");
 
@@ -11,6 +12,32 @@ testSuite.add("reduce", assert => {
     assert.is(join(names), "Florian Katrin Nils");
 
     assert.is(join([]), "");
+});
+
+testSuite.add("splice", assert => {
+    let names = ["Florian","Katrin","Nils"];
+    names.splice(0,1);
+    assert.isTrue(names.eq(["Katrin","Nils"]));
+
+    names = ["Florian","Katrin","Nils"];
+    names.splice(0,0);
+    assert.isTrue(names.eq(names));
+
+    names = ["Florian","Katrin","Nils"];
+    names.splice(0,0, "Aaron","Manuel");
+    assert.isTrue(names.eq(["Aaron","Manuel","Florian","Katrin","Nils"]));
+});
+
+testSuite.add("callback", assert => {
+
+    const benchmark = callback => {
+        const start = 0;
+        callback();
+        const end   = 10;
+        return end - start;
+    }
+    assert.is( benchmark( () => 1+1 ), 10 );
+
 });
 
 testSuite.run();
